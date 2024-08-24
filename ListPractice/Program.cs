afdad8c50516b6e1d39b71321ec8acf5094eef4e
Safayat Hossain <safayathossain40@gmail.com>
2024-08-24 11:06:42 +0600

    Array of Multiples added

--- ListPractice/Program.cs
+++ ListPractice/Program.cs
@@ -6,7 +6,7 @@ public class Program
     public static void Main()
     {
         List<int> number = new List<int>();
-        for (int i = 0; i < 10; i++)
+        for (int i = 1; i <= 21; i++)
         {
             number.Add(i);
         }
@@ -14,7 +14,7 @@ public class Program
         List<int> oddNumber = new List<int>();
         List<int> evenNumber = new List<int>();
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 1; i <= 20; i++)
         {
             if (number[i] % 2 == 0)
             {
